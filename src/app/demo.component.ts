f439ce405b464b243d4ad9978ce3956bdf1e01cd
DM-be <dennis.morent@student.howest.be>
2020-10-14 13:14:22 +0200

    1 file modified

--- src/app/demo.component.ts
+++ src/app/demo.component.ts
@@ -1,39 +1,46 @@
-import { AfterViewInit, Component, ElementRef,  Input, OnInit,  ViewChild } from '@angular/core';
-import { fromEvent } from 'rxjs';
-import { map, throttleTime } from 'rxjs/operators';
-
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild
+} from "@angular/core";
+import { fromEvent } from "rxjs";
+import { map, throttleTime } from "rxjs/operators";
 
 interface MouseClick {
   x: number;
   y: number;
 }
 
-
-
 @Component({
-  selector: 'demo',
-  template: ` <button >Click me</button>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "demo",
+  template: `
+    <button>Click me</button>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-
-
 export class DemoComponent implements OnInit {
-
-
   ngOnInit(): void {
-    const button: HTMLButtonElement = document.querySelector('button');
-   // button.addEventListener('click', (e: MouseEvent) => console.log(e.clientX))
-    // now with an observable
+    const button: HTMLButtonElement = document.querySelector("button");
+    button.addEventListener("click", (e: MouseEvent) => console.log(e.clientX));
 
-   // fromEvent(button, 'click').subscribe((e: MouseEvent) => console.log(e.clientX) )
+    // lets convert this to an observables
 
-    // but why use observables? We have eventlisteners so...
+    // but why use observables? Eventlisteners do the same thing?
     // the power comes from operators!
     // we have a stream that we can manipulate to our every needs using operators
-    
 
-    // we are only interested in in a mouse click event every 1 second
-    // using event listeners: 
+    // what if we are only interested in in a mouse click event every 1 second?
+    // using event listeners we need the following:
+    // (don't forget to remove the previous event listener)
 
     // let rate = 1000;
     // let lastClick = Date.now() - rate;
@@ -41,40 +48,17 @@ export class DemoComponent implements OnInit {
     // if (Date.now() - lastClick >= rate) {
     //   lastClick = Date.now();
     //   console.log(e.clientX);
-      
-    
     // }
+    // })
 
     // so alot of code for something relatively easy
     // with observables we can view the events as a stream we can "funnel"
     // we can extract only data we are interested in through this funnel
     // this is done by using .pipe() method on the observable
-    const observable = fromEvent(button, 'click');
-    observable.pipe(
-      throttleTime(1000)
-      ).subscribe((e: MouseEvent) => console.log(e))
 
     // now let's say we want to change the event into a custom object
     // lets define an interface --> mouseclicked
     // map needs to return a new observable
     // we can hard type the values ;)
-
-    fromEvent(button, 'click').pipe(
-      throttleTime(1000),
-      map((e: MouseEvent) => {
-        return {
-          x: e.clientX,
-          y: e.clientY,
-        } as MouseClick
-      }) 
-    ).subscribe((mouseClickEveryOneSecond: MouseClick) => console.log(mouseClickEveryOneSecond))
-
-
-
   }
-
-  
-    
-
-  
 }
